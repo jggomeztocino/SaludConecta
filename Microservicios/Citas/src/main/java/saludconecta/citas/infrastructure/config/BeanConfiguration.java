1b79f84711789f20765307180953c77298790a12
Jesús Gómez <90068817+jggomeztocinouca@users.noreply.github.com>
2024-09-11 23:27:54 +0200

    compose stacks adjustment & interconnection + mongodb adapters

--- Microservicios/Citas/src/main/java/saludconecta/citas/infrastructure/config/BeanConfiguration.java
+++ Microservicios/Citas/src/main/java/saludconecta/citas/infrastructure/config/BeanConfiguration.java
@@ -1,15 +1,23 @@
 package saludconecta.citas.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.Configuration;
 import saludconecta.citas.domain.ports.CitaRepository;
 import saludconecta.citas.domain.ports.EventPublisher;
+import saludconecta.citas.domain.ports.ConsultaRepository;
 import saludconecta.citas.domain.service.CitaService;
+import saludconecta.citas.domain.service.ConsultaService;
 
+@Configuration
 public class BeanConfiguration {
 
     @Bean
     public CitaService citaService(CitaRepository citaRepository, EventPublisher eventPublisher) {
         return new CitaService(citaRepository, eventPublisher);
+    }
+
+    @Bean
+    public ConsultaService consultaService(ConsultaRepository consultaRepository, EventPublisher eventPublisher) {
+        return new ConsultaService(consultaRepository, eventPublisher);
     }
 }
--- Microservicios/Citas/src/main/java/saludconecta/citas/infrastructure/config/MongoConfig.java
+++
@@ -1,5 +0,0 @@
-package saludconecta.citas.infrastructure.config;
-
-public class MongoConfig {
-    // Mongo configuration
-}
